e0b46b6b4021dbc939db3087c4ecf7ddac7a102a
ItsMeFadz <fadhilahruhiyah03@gmail.com>
2024-12-03 04:38:26 -0800

    final

---
+++ 6.string-manipulation/concenation.js
@@ -0,0 +1,13 @@
+// concenat
+
+const username = "fadil ruhiyah";
+
+const greating = "hello"
+
+const fristuppercase = username.charAt(0).toUpperCase();
+
+const sisanama = username.slice(0,5);
+
+// console.log(username+greating)
+
+console.log(username.concat(greating));
---
+++ 6.string-manipulation/method.js
@@ -0,0 +1,9 @@
+// const username = "theo";
+
+// const greating = "hello";
+
+// console.log(username+greating);
+
+let x = "enigma";
+let y = x.slice(1).charAt(0).toUpperCase();
+console.log("E"+y);
